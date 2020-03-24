b5e52a647f2847b0b09a110fc45505fea4ec923a
Jason <772640794@qq.com>
2020-03-25 00:34:03 +0800

    update test

--- test.sh
+++ test.sh
@@ -3,7 +3,9 @@
 ./richochet_robots ./puzzle1.txt -max_moves 3 -visualize C
 ./richochet_robots ./puzzle1.txt -max_moves 9 -visualize A
 ./richochet_robots ./puzzle1.txt -all_solutions
+./richochet_robots ./puzzle1.txt # one solution
 ./richochet_robots ./puzzle1.txt -max_moves 6 -all_solutions
 ./richochet_robots ./puzzle1.txt -max_moves 5 -all_solutions
 ./richochet_robots ./puzzle2.txt -max_moves 6 -all_solutions
 ./richochet_robots ./puzzle2.txt -max_moves 5 -all_solutions
+./richochet_robots ./puzzle2.txt # one solution
